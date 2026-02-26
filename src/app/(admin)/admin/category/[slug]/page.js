d0c13efd094d4b076f879fdb698d82379e1ffe6e
Keshav <keshavkhandelwal30@gmail.com>
2026-02-26 15:40:16 +0530

    Meta Title Changed

--- src/app/(admin)/admin/category/[slug]/page.js
+++ src/app/(admin)/admin/category/[slug]/page.js
@@ -1,4 +1,24 @@
 import AdminCategoryComponent from "@/components/AdminCategoryComponent";
+import api from "@/lib/api";
+
+export const generateMetadata = async ({ params }) => {
+    const slug = (await params)?.slug;
+    if (slug === "") return { title: "All" };
+
+    const baseUrl = new URL(process.env.NEXT_PUBLIC_BASE_URL);
+    const apiPath = process.env.NEXT_PUBLIC_CATEGORY_API.replace(
+        "catSlug",
+        slug,
+    );
+    baseUrl.pathname = apiPath;
+    const fullUrl = baseUrl.toString();
+
+    const catResponse = await api.get(fullUrl);
+    const name = catResponse?.data?.category?.name;
+    return {
+        title: name.charAt(0).toUpperCase() + name.slice(1),
+    };
+};
 
 export default async function CategoryPage({ params, searchParams }) {
     const slug = (await params)?.slug;
